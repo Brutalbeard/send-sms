8e108a01e20df6b2e0c38b48ffee2a5ab102ed2e
John Celoria <jceloria@gmail.com>
2019-12-17 11:45:52 -0500

    first push

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # send-sms
+
+## About
+This application will log into your RingCentral account (using password login flow), and send a test SMS from each of your extension's phone numbers. 
+
+The list of phone numbers that are pulled from the RC account may or may not have the SendSMS feature enabled on them. If the phone number doesn't have that feature, it's skipped over. 
+
+## Installation
+Clone repo or download as zip and open. 
+
+Change app root dir
+```
+$ cd /path/to/app
+```
+Install dependencies (this app is written using JavaScript and will requires [Node](nodejs.org)
+```
+$ npm install
+```
+Make a copy of ```env_example``` and rename it to ```.env```. Then fill in the appropriate fields. 
+```
+RINGCENTRAL_USERNAME=
+RINGCENTRAL_EXTENSION=
+RINGCENTRAL_PASSWORD=
+
+RINGCENTRAL_CLIENT_ID=
+RINGCENTRAL_CLIENT_SECRET=
+RINGCENTRAL_PLATFORM_ADDRESS=https://platform.ringcentral.com
+
+TO_NUMBER= # This is the phone number you want to send the SMS to
+```
+
+## Run the app
+Once everything is filled in...
+```
+$ node .
+```
+Each message will go out from the phone number and will say "Test Message {iterator}".
---
+++ index.js
@@ -0,0 +1,62 @@
+const dotenv = require('dotenv')
+const SDK = require('@ringcentral/sdk').SDK
+
+dotenv.config()
+
+const rcsdk = new SDK({
+    server: process.env.RINGCENTRAL_PLATFORM_ADDRESS,
+    clientId: process.env.RINGCENTRAL_CLIENT_ID,
+    clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+    handleRateLimit: true
+})
+
+rcsdk.platform().on(rcsdk.platform().events.rateLimitError, () => {
+    console.log("Hit rate limit...")
+})
+
+rcsdk.login({
+    username: process.env.RINGCENTRAL_USERNAME, // phone number in full format
+    extension: process.env.RINGCENTRAL_EXTENSION, // leave blank if direct number is used
+    password: process.env.RINGCENTRAL_PASSWORD
+}).then(() => {
+    console.log("Logged in successfully")
+    main()
+}).catch(e => {
+    alert(e.message || 'Server cannot authorize user');
+});
+
+async function main() {
+
+    let fromNumbers = await rcsdk.get('/restapi/v1.0/account/~/extension/~/phone-number', {
+        perPage: 1000
+    }).then(res => {
+        return res.json()
+    }).then(res => {
+        return res.records
+    }).catch(e => {
+        console.error("Issue pulling user's phone number list: ", e)
+    })
+
+    let i = 1
+    for (let record of fromNumbers) {
+        if (!record.features.includes('SmsSender')) { continue }
+
+        await rcsdk.post('/restapi/v1.0/account/~/extension/~/sms', {
+            from: {
+                phoneNumber: record.phoneNumber
+            },
+            to: [
+                {
+                    phoneNumber: process.env.TO_NUMBER
+                }
+            ],
+            text: `Test message ${i}`
+        }).then(() => {
+            console.log(i)
+            i += 1
+        }).catch(e => {
+            console.error("Issue sending an sms: ", e.message)
+        })
+    }
+
+}
